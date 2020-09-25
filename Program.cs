2c1e214060dde559082280bb227fc98756bab9c7
a2404045631 <71770764+a2404045631@users.noreply.github.com>
2020-09-25 23:14:13 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "枪的描述";
+            string gunName;
+            int gunCount;
+            Console.WriteLine("请输入枪的名字");
+            gunName = Console.ReadLine();
+            string str = string.Format("枪\n的名字为:{0}", gunName);
+            Console.WriteLine(str);
+
+        }   
+    }
+}
